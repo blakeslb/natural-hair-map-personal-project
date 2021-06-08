225feac5a97fd18c7e675bd8822fdb17971f1ff3
Bianca Blakesley <bianca.blakesley@gmail.com>
2021-06-08 14:29:22 -0400

    adds isApproved condition to map markers

---
+++ routes/adminRouter 2.js
@@ -0,0 +1,28 @@
+const express= require('express');
+const router = express.Router();
+const adminController = require('../controllers/admincontroller');
+
+
+
+router.route('/')
+    //admin page
+    .get(adminController.admin_get)
+    //listings submitted by user go here
+    // .post(adminController.admin_post)
+
+// router.route('/review')
+    //admin review page (has form)
+    .get(adminController.review_get);
+    //
+    //.put(adminController.review_put);
+
+    router.route('/add')
+    .get(adminController.admin_add)
+    .post(adminController.admin_post);
+
+
+    module.exports = router; 
+
+    
+
+
---
+++ routes/siteRouter 2.js
@@ -0,0 +1,31 @@
+const express= require('express');
+const router = express.Router();
+const siteController = require('../controllers/sitecontroller');
+
+
+router.route('/')
+    .get(siteController.index_get)
+    
+    // .post(siteController.index_post);
+
+// router.route('/:id')
+    // .post(siteController.approveListing)
+    // .put(siteController.updateListing);
+
+router.route('/about')
+    .get(siteController.about_get);
+
+router.route('/contact')
+    .get(siteController.contact_get)
+//     // .post()
+
+router.route('/add')
+    .get(siteController.add)
+//     .post(siteController.add_post);
+    .post(siteController.add_post);
+
+router.route('/submission-confirmation')
+    .get(siteController.confirm);
+
+
+module.exports = router; 
