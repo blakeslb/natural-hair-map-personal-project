cc52f6c8c131af3c14d4061d8f071b704aee0c0e
Bianca Blakesley <bianca.blakesley@gmail.com>
2021-06-12 20:00:14 -0400

    merge with version7

--- controllers/admincontroller.js
+++ controllers/admincontroller.js
@@ -1,78 +1,95 @@
 const { response } = require('express');
 const Stylist = require('../models/stylistModel');
+const User = require("../models/adminModel");
+
+
 
 module.exports = {
-
-
-
 
     //admin page
 
     admin_get: (request, response) => {
-        
+        if (request.isAuthenticated()) {
+
+
             Stylist.find({}, (error, allStylists) => {
-              if (error) {
-                return error;
-            } else {
-                response.render('pages/admin', { data: allStylists });
-
-            }
+                if (error) {
+                    return error;
+                } else {
+                    response.render('pages/admin', { data: allStylists });
+                }
             })
-          
-          
+        } else {
+            response.redirect("/")
+        }
+
     },
 
-    //about page
-    admin_post: (request, response) => {
 
-        
-        response.render('pages/about');
-    },
+    delete: (request, response) => {
+        if (request.isAuthenticated()) {
 
-    review_get: (request, response) => {
-        response.render('admin-review')
+            const { id } = request.params;
+            Stylist.deleteOne({ _id: id }, (error) => {
+                if (error) {
+                    return error;
+                } else {
+                    response.redirect('/admin/')
+                }
+            })
+        } else {
+            response.redirect('/')
+        }
     },
+    approve: (request, response) => {
+        if (request.isAuthenticated()) {
 
-    admin_add: (request, response) => {
-        response.send('testing my geocode app!')
-    },
-    approve: (request, response) =>  {
-        const stylistId = request.params.id;
-        Stylist.findOne({_id: stylistId}, (error, foundStylist) => {
-            if(error) {
-                return error;
-            }else {
-                response.render('pages/stylist', { stylist: foundStylist });
-            }
-        })
+            const stylistId = request.params.id;
+            Stylist.findOne({ _id: stylistId }, (error, foundStylist) => {
+                if (error) {
+                    return error;
+                } else {
+                    response.render('pages/stylist', { stylist: foundStylist });
+                }
+            })
+        } else {
+            response.redirect('/')
+        }
     },
     approve_put: (request, response) => {
+        if (request.isAuthenticated()) {
+
         const { id } = request.params;
 
-		Stylist.findByIdAndUpdate(id, {$set: {
-		    fullName: request.body.fullName,
-            business_type: request.body.business_type,
-            website: request.body.website,
-            address: request.body.address,
-            coords: request.body.coords,
-            phone: request.body.number,
-            instagram: request.body.instagram,
-            facebook: request.body.facebook,
-            tiktok: request.body.tiktok,
-            twitter: request.body.twitter,
-            isApproved: (request.body.isApproved  === "true")
+        Stylist.findByIdAndUpdate(id, {
+            $set: {
+                fullName: request.body.fullName,
+                business_type: request.body.business_type,
+                website: request.body.website,
+                address: request.body.address,
+                coords: request.body.coords,
+                phone: request.body.number,
+                instagram: request.body.instagram,
+                facebook: request.body.facebook,
+                tiktok: request.body.tiktok,
+                twitter: request.body.twitter,
+                isApproved: (request.body.isApproved === "true")
 
-		}}, {new: true}, error => {
-			if(error) {
+            }
+        }, { new: true }, error => {
+            if (error) {
                 console.log(error)
-				return error; 
-			}else {
-				response.redirect('/admin')
+                return error;
+            } else {
+                response.redirect('/admin')
                 // response.send(request.body);
-			}
-		})
+            }
+        })
+    } else {
+    response.redirect("/")
+}
+    
 
-
-    }
+}
 }
 
