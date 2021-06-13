81cd68c05eb2e152ac33befdcce8240ff680f904
Bianca Blakesley <bianca.blakesley@gmail.com>
2021-06-13 17:31:44 -0400

    adds models folder

---
+++ models/adminModel.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+
+    },
+    password: {
+        type: String,
+
+    },
+    googleId: {
+        type: String
+    }
+});
+
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
+
+const User = mongoose.model('User', userSchema)
+passport.use(User.createStrategy());
+
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/admin"
+},
+    function (accessToken, refreshToken, email, cb) {
+        console.log(email);
+        User.findOrCreate({ googleId: email.id, username: email.displayName }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
+
+module.exports = User;
